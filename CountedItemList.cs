15c4d501c8fe017b4880068d094642fba67c3654
Henno0701 <1048091@hr.nl>
2023-02-21 15:48:51 +0100

    Working world.cs

--- CountedItemList.cs
+++ CountedItemList.cs
@@ -1,3 +1,4 @@
+
 public class CountedItemList
 {
   public List<CountedItem> TheCountedItemList;
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        var rat = new Monster(1, "rat", "rats", 5, 3, 10, 3, 3);
+        Console.WriteLine(rat.NamePlural);
+    }
+}
