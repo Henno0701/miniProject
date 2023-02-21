7a1dfda2d4f25321207153b95736e0c1622b77fc
joelhogeschool <1062069@hr.nl>
2023-02-21 15:28:21 +0100

    Update Monster.cs

--- Monster.cs
+++ Monster.cs
@@ -1,25 +1,25 @@
 public class Monster
 {
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public string NamePlural { get; set; }
-    public int MaximumDamage { get; set; }
-    public int RewardExperience { get; set; }
-    public int RewardGold { get; set; }
-    public List<CountedItemList> Loot;
-    public int MaximiumHitPoints { get; set; }
-    public int CurrentHitPoints { get; set; }
+  public int ID { get; set; }
+  public string Name { get; set; }
+  public string NamePlural { get; set; }
+  public int MaximumDamage { get; set; }
+  public int RewardExperience { get; set; }
+  public int RewardGold { get; set; }
+  public CountedItemList Loot;
+  public int MaximiumHitPoints { get; set; }
+  public int CurrentHitPoints { get; set; }
 
-    public Monster(int id, string name, string namePlural, int maximumDamage,
-                   int rewardExperience, int rewardGold, int maximiumhitpoints, int currentHitPoints)
-    {
-        ID = id;
-        Name = name;
-        NamePlural = namePlural;
-        MaximumDamage = maximumDamage;
-        RewardExperience = rewardExperience;
-        RewardGold = rewardGold;
-        maximiumhitpoints = maximiumhitpoints;
-        CurrentHitPoints = currentHitPoints;
-    }
+  public Monster(int id, string name, string namePlural, int maximumDamage,
+                 int rewardExperience, int rewardGold, int maximiumhitpoints, int currentHitPoints)
+  {
+    ID = id;
+    Name = name;
+    NamePlural = namePlural;
+    MaximumDamage = maximumDamage;
+    RewardExperience = rewardExperience;
+    RewardGold = rewardGold;
+    maximiumhitpoints = maximiumhitpoints;
+    CurrentHitPoints = currentHitPoints;
+  }
 }
