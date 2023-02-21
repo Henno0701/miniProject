c74f2d221af645ec6adb3981284ed699f6b0c8d1
Henno0701 <1048091@hr.nl>
2023-02-21 14:02:14 +0100

    Added Quest classes

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Player
+{
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximiumHitPoints;
+    public int Gold;
+    public int ExperiencePoints;
+    public Weapon CurrentWeapon;
+    public Location CurrentLocation;
+    public QuestList QuestLog;
+    
+}
---
+++ PlayerQuest.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class PlayerQuest
+{
+    public Quest TheQuest;
+    public bool IsCompleted;
+}
